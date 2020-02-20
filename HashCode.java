18942939c59a442618ec04320db401865026a068
Aman Gupta <48331596+Aman23Gupta@users.noreply.github.com>
2020-02-20 19:33:42 +0530

    stratercode

---
+++ HashCode.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Code {
+    public static void main(String[] args)throws IOException {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        
+        //code here
+        
+        out.flush();
+        out.close();
+    }
+
+    static class Scanner {
+        StringTokenizer st;
+        BufferedReader br;
+
+        public Scanner(InputStream s) {
+            br = new BufferedReader(new InputStreamReader(s));
+        }
+
+        public Scanner(FileReader fileReader) {
+            br = new BufferedReader(fileReader);
+        }
+
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens())
+                st = new StringTokenizer(br.readLine());
+            return st.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        public boolean ready() throws IOException {
+            return br.ready();
+        }
+    }
+}
